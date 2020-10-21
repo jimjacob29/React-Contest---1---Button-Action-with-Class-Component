96ee050c93b5bcc0694a7fa066c12f80bf77f6b9
JIMMY JACOB <68079806+jimjacob29@users.noreply.github.com>
2020-10-21 21:08:44 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,29 @@
 import React, { Component, useState } from "react";
-import "../styles/App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: ""
+      text: "",
+      show: false
     };
   }
+  handleClick = () => {
+    this.setState({
+      text: `Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy`
+    });
+    this.setState({
+      show: true
+    });
+  };
   render() {
     return (
       <div id="main">
         {/* Do not remove this main div!! */}
-        <button
-          id="click"
-          onClick={() =>
-            this.setState({
-              text: `Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy`
-            })
-          }
-        >
+        <button id="click" onClick={this.handleClick}>
           click
         </button>
-        {this.state.text ? <p id="para">{this.state.text}</p> : null}
+        {this.state.show ? <p id="para">{this.state.text}</p> : null}
       </div>
     );
   }
